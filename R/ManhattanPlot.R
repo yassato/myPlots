6f368a8fa21b70a987a1f224925ba505aa1abb45
Yasuhiro Sato <yassato36c@outlook.com>
2021-11-08 17:18:27 +0100

    Add files via upload

---
+++ R/ManhattanPlot.R
@@ -0,0 +1,37 @@
+#' Depicting Manhattan plots for GWAS without ggplot2
+#'
+#' A simple plot function to draw Manhattan plot for GWAS results.
+#' @param chr Chromosome number. It should have the same length as \code{pos} and \code{p}.
+#' @param pos Positions on the chromosomes.
+#' @param p A vector of p-values ranging from 0 to 1.
+#' @param ... Other arguments passed to plot().
+#' @author Yasuhiro Sato (\email{sato.yasuhiro.36c@kyoto-u.jp})
+#' @export
+ManhattanPlot = function(chr, pos, p, ...) {
+  if(length(pos)!=length(chr)) stop("chr and pos length differ")
+  if(length(chr)!=length(p)) stop("chr and p length differ")
+
+  chr <- as.factor(chr)
+  coord <- 0
+  M <- 0
+  tic <- numeric(nlevels(chr))
+  for (i in 1:nlevels(chr)) {
+    w <- (chr == levels(chr)[i])
+    pos.c <- pos[w]
+    coord[w] <- M + pos.c
+    mx <- max(pos.c)
+    tic[i] <- M + mx/2
+    M <- M + mx
+  }
+  coord <- coord/M
+  tic <- tic/M
+
+  th <- 0.05/length(p)
+  args <- list(...)
+  args$xaxt <- "n"
+  args$bty <- "n"
+  args$x <- coord
+  args$y <- -log10(p)
+  do.call(plot,args)
+  abline(h=-log10(th),lty=2,col="grey")
+}
---
+++ R/qqPlot.R
@@ -0,0 +1,20 @@
+#' Depicting QQ-plot for GWAS without ggplot2
+#'
+#' A simple plot function to draw a QQ-plot for GWAS results.
+#' @param p A vector of p-values ranging from 0 to 1.
+#' @param ... Other arguments passed to plot().
+#' @author Yasuhiro Sato (\email{sato.yasuhiro.36c@kyoto-u.jp})
+#' @export
+qqPlot = function(p,...) {
+  o <- -log(sort(p,decreasing=FALSE),10)
+  e <- -log(stats::ppoints(length(p)),10)
+
+  args <- list(...)
+  args$x <- e
+  args$y <- o
+  args$bty <- "n"
+  args$xlab <- "Expected -log10(P)"
+  args$ylab <- "Observed -log10(P)"
+  do.call(plot,args)
+  abline(a=0,b=1,lty=2,col="grey")
+}
